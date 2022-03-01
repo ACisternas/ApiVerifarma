a989e7c1393023b7defac4383c4f38bd9d05caf1
aleecister <aleecister@gmail.com>
2022-03-01 16:55:53 -0300

    metodo getfarmaciamascercana

--- app/Http/Controllers/FarmaciaController.php
+++ app/Http/Controllers/FarmaciaController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Log;
 class FarmaciaController extends Controller
 {
     /**
-     * Store a newly created resource in storage.
+     * Crear farmacia.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -32,7 +32,7 @@ class FarmaciaController extends Controller
     }
 
     /**
-     * Show.
+     * getFarmacia.
      *
      * @param  $id
      * @return \Illuminate\Http\Response
@@ -49,6 +49,53 @@ class FarmaciaController extends Controller
             Log::debug($th->getMessage());
             return response()->json(['Error al obtener farmacia: '=>$th->getMessage()], 500);
         }
+    }
+
+    /**
+     * getFarmaciaCercana.
+     *
+     * @param  $lat, $lon
+     * @return \Illuminate\Http\Response
+    */
+    public function getFarmaciaCercana($lat, $lon)
+    {
+        try {
+            $farmacias = Farmacia::all();
+            $farmaciaMasCercana = $farmacias[0];
+            $distanciaMinima = $this->calcularDistancia($lat, $lon, Farmacia::first()->latitud, Farmacia::first()->longitud);
+
+            foreach ($farmacias as $f) {
+
+                $distancia = $this->calcularDistancia($lat, $lon, $f->latitud, $f->longitud);
+                if ($distancia < $distanciaMinima) {
+                    $distanciaMinima = $distancia;
+                    $farmaciaMasCercana = $f;
+                }
+            }
+            return response()->json(['Farmacia mas cercana'=>$farmaciaMasCercana], 200);
+            //return  response()->json(['Farmacia mas cercana'=>$distanciaMinima], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['message'=> 'Error al obtener farmacias: '.$th->getMessage()], 500);
+            Log::debug($th->getMessage());
+        }
+    }
+
+    private function calcularDistancia($lat0, $lon0, $lat1, $lon1)
+    {
+        //Transformo a radianes;
+        $rlat0 = deg2rad($lat0);
+        $rlon0 = deg2rad($lon0);
+        $rlatF = deg2rad($lat1);
+        $rlonF = deg2rad($lon1);
+
+        //diferencia entre estos valores
+        $latDelta = $rlatF - $rlat0;
+        $lonDelta = $rlonF - $rlon0;
+
+        //finalmente calculo la distancia en kilometros
+        $distanciaKm = (6371 * acos(cos($rlat0) * cos($rlatF) * cos($lonDelta) + sin($rlat0) * sin($rlatF)));
+
+        return $distanciaKm;
     }
 
 }
